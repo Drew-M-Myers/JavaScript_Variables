3305da1ce7625d7892b55fdc35dcb69a702f9f0e
Drew Myers <drew.myers59@gmail.com>
2022-10-04 03:02:28 -0400

    modified:   app.js

--- app.js
+++ app.js
@@ -3,8 +3,36 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
-
-// Exercise 2
-
+// Exercise 1 and 2
+const firstName = "Drew";
+let lastName = "Myers";
+var age = 25;
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+let fullName = firstName + "" + lastName;
+console.log("Full Name:", fullName);
+
+let myStory;
+let city = "McSherrystown";
+let pastime = "hanging out with firends, disc golf, video games";
+
+myStory = 
+`My name is ${fullName}, I live in ${city}. I like${pastime}and coding.`;
+ 
+console.log("My story:", myStory);
